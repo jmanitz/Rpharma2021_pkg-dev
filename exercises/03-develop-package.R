aad36d5417f580eaa571cfc33b46fa721005f49d
jmanitz <juliane.manitz@emdserono.com>
2021-10-22 17:09:27 -0400

    more updates

--- exercises/03-develop-package.R
+++ exercises/03-develop-package.R
@@ -1,9 +1,89 @@
 # --- Part II ------------------------------------------------------------------
-# Juliane - please add whatever you'd like to this file!
+
+#install.packages(c("devtools", "roxygen2", "usethis","fs"))
+require(usethis)
+require(roxygen2)
+require(devtools)
+
+# --- Preparation --------------------------------------------------------------
+
+create_package("../../myutils", open = TRUE)
+
+edit_file("DESCRIPTION")
 
 # --- I have a package structure, now what? ------------------------------------
 
-use_r("my_func")    # creates and opens R files, shortcut to open R files, ctrl + .
+# --- Write R code -------------------------------------------------------------
+
+# define some basic functions
+add <- function(x, y){ x + y }
+plusone <- function(x){ x + 1 }
+
+# test your source code
+add(10, 1)
+plusone(4)
+
+# create and open R file, shortcut to open R files, ctrl + .
+use_r("math")
+
+# --- Example Data -------------------------------------------------------------
+
+# prepare some example data
+dat <- data.frame(id=1:10, x=rpois(10, 5), y=rpois(10, 5))
+
+# examples
+add(dat$x, dat$y)
+plusone(dat$y)
+
+# Save data in the package
+use_data(dat)
+
+# --- Example Data -------------------------------------------------------------
+
+# \name{add}
+# \alias{add}
+# \title{Add together two numbers}
+# \usage{ add(x, y) }
+# \arguments{
+#   \item{x}{A number}
+#   \item{y}{A number}
+# }
+# \value{
+#   The sum of \code{x} and \code{y}
+# }
+# \description{ Add together two numbers }
+# \examples{
+#   add(1, 1)
+#   add(10, 1)
+#
+#   data(dat)
+#   add(dat$x, dat$y)
+# }
+
+
+## roxygen2
+
+#' Add together two numbers.
+#'
+#' @param x A number.
+#' @param y A number.
+#' @return The sum of \code{x} and \code{y}.
+#' @examples
+#'   add(1, 1)
+#'   add(10, 1)
+#'
+#'   data(dat)
+#'   add(dat$x, dat$y)
+#' @export
+add <- function(x, y) {
+  x + y
+}
+
+roxygenize()
+getwd()
+
+# --- Unit Testing -------------------------------------------------------------
+
 use_test("my_func")
 
 # --- Tools to manage your DESCRIPTION and NAMESPACE ---------------------------
---
+++ exercises/math.R
@@ -0,0 +1,31 @@
+# define some basic functions
+
+
+#' Add together two numbers.
+#'
+#' @param x A number.
+#' @param y A number.
+#' @return The sum of \code{x} and \code{y}.
+#' @examples
+#'   add(1, 1)
+#'   add(10, 1)
+#'
+#'   data(dat)
+#'   add(dat$x, dat$y)
+#' @export
+add <- function(x, y) {
+  x + y
+}
+
+#' Add 1 to a number
+#'
+#' @param x A number
+#' @return The sum of \code{x + 1}
+#' @examples
+#' plusone(13)
+#'
+#' data(dat)
+#' plusone(dat$y)
+plusone <- function(x){
+  x + 1
+}
